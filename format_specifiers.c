e62a6ddfeece64d7f76015d5d292dd6ef7620071
F_O_P <tayler.coon@gmail.com>
2023-03-08 12:52:15 -0800

    Setup format_specifers

---
+++ format_specifiers.c
@@ -0,0 +1,54 @@
+#include "MAIN_H"
+
+/**
+ * print_char - print character
+ * @va_list: fill
+ *
+ * Return: Always 0
+ */
+
+int print_char(va_list)
+{
+	_putchar(va_arg(list, int));
+	return (1);
+}
+
+/**
+ * print_string - print string
+ * @va_list: fill
+ *
+ * Return: Always 0
+ */
+
+int print_string(va_list)
+{
+	
+}
+
+/**
+ * print_percent - print percent
+ * @va_list: fill
+ *
+ * Return: Always 0
+ */
+
+int print_percent(va_list)
+{
+	
+}
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+
+/**
+ * 
+ * 
+ *
+ * 
+ */
+
--- main.h
+++ main.h
@@ -23,7 +23,6 @@ typedef struct convert
 
 int _printf(const char *format, ...);
 int _putchar(char c);
-int _write_char(char);
 int print_char(va_list);
 int print_sting(va_list);
 int print_percent(va_list);
@@ -33,8 +32,8 @@ int print_binary(va_list);
 int print_reversed(va_list arg);
 int print_rot13(va_list);
 int unsigned_integer(va_list);
-int print_hex(va_list);
-int print_octal(va_list);
-int print_heX(va_list);
+int print_hex(va_list, list);
+int print_octal(va_list, list);
+int print_heX(va_list, list);
 
 #endif
